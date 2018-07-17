cbd2414b2a498fa23595dbd6ba21aafa4c8b6d0d
THT <21095057@student.uwa.edu.au>
2018-07-17 17:26:54 +0800

    4.2

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -6,10 +6,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace WishList.Data
 {
-    public class ApplicationDbContext :DbContext
+    public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions options) : base (options)
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
 
         }
+    }
 }
